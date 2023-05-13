2a33117df07c43191894a87add0eacab3f28c7d9
Miklós Antal Werner <wernermiklos@gmail.com>
2023-05-13 10:45:36 +0200

    Some cleaning and ordering steps

--- core/utilities/generate_Etensor.m
+++
@@ -1,7 +0,0 @@
-function Etensor = generate_Etensor(sym, progress_info)
-%GENERATE_ETENSOR Summary of this function goes here
-%   Detailed explanation goes here
-
-
-end
-
--- pathimport.m
+++ pathimport.m
@@ -1,9 +1,5 @@
 addpath(genpath('core'));
-addpath(genpath('ORS_interface'));
-addpath test_scripts;
-addpath 3rdquant;
-%addpath D:\TEBD_simulation;
-addpath Patrik_playground;
+addpath run_scripts;
 
 
 
